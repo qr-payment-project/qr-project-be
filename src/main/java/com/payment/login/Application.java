66bfdfcc21f1876758f7be9c6cec56f0b58832ea
ruudska6 <feels19@naver.com>
2025-08-17 15:55:16 +0900

    first init

--- src/main/java/com/payment/login/Application.java
+++ src/main/java/com/payment/login/Application.java
@@ -1,13 +1,13 @@
-package com.example.backend;
+package com.payment.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
